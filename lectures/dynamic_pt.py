34dbc88222676517320ec210cfc38d884f720e17
vuhoangminh <minhmanutd@gmail.com>
2020-02-06 00:28:33 +0100

    [MINH] finalize codes for lectures introduction to Keras and PyTorch

--- lectures/dynamic_pt.py
+++ lectures/dynamic_pt.py
@@ -1,26 +1,31 @@
 import torch
 
-dtype = torch.float
-device = torch.device("cpu")
-
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
+# N is batch size; 
+# D_in is input dimension;
+# H is hidden dimension; 
+# D_out is output dimension.
 N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create random Tensors to hold input and outputs.
+# create random Tensors to hold input and outputs.
+dtype, device = torch.float, torch.device("cpu")
 x = torch.randn(N, D_in, device=device, dtype=dtype)
 y = torch.randn(N, D_out, device=device, dtype=dtype)
 
-# Create random Tensors for weights.
-w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
-w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+# create random Tensors for weights.
+w1 = torch.randn(D_in, H, device=device, 
+                    dtype=dtype, requires_grad=True)
+w2 = torch.randn(H, D_out, device=device, 
+                    dtype=dtype, requires_grad=True)
 
+# setup training
 learning_rate = 1e-6
+
+# execute the graph
 for t in range(500):
-    # Forward pass
+    # forward pass
     y_pred = x.mm(w1).clamp(min=0).mm(w2)
 
-    # Compute and print loss 
+    # compute loss 
     loss = (y_pred - y).pow(2).sum()
     if t % 100 == 99:
         print(t, loss.item())
@@ -28,11 +33,12 @@ for t in range(500):
     # backward pass
     loss.backward()
 
-    # Manually update weights using gradient descent
+    # update weights using gradient descent
     with torch.no_grad():
         w1 -= learning_rate * w1.grad
         w2 -= learning_rate * w2.grad
 
-        # Manually zero the gradients after updating weights
+        # manually zero the gradients 
+        # after updating weights
         w1.grad.zero_()
         w2.grad.zero_()
--- lectures/static_tf.py
+++ lectures/static_tf.py
@@ -1,48 +1,54 @@
 import tensorflow as tf
 import numpy as np
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
+# N is batch size;
+# D_in is input dimension;
+# H is hidden dimension;
+# D_out is output dimension.
 N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create placeholders for the input and target data
-x = tf.placeholder(tf.float32, shape=(None, D_in))
-y = tf.placeholder(tf.float32, shape=(None, D_out))
+# create placeholders
+x = tf.placeholder(tf.float32,
+                   shape=(None, D_in))
+y = tf.placeholder(tf.float32,
+                   shape=(None, D_out))
 
-# Create Variables for the weights
+# create Variables for the weights
 w1 = tf.Variable(tf.random_normal((D_in, H)))
 w2 = tf.Variable(tf.random_normal((H, D_out)))
 
-# Forward pass
+# setup training
+learning_rate = 1e-6
+
+# forward pass
 h = tf.matmul(x, w1)
 h_relu = tf.maximum(h, tf.zeros(1))
 y_pred = tf.matmul(h_relu, w2)
 
-# Compute loss
+# compute loss
 loss = tf.reduce_sum((y - y_pred) ** 2.0)
 
-# Compute gradient of the loss
+# compute gradient of the loss
 grad_w1, grad_w2 = tf.gradients(loss, [w1, w2])
 
-# Update the weights using gradient descent
-learning_rate = 1e-6
+# backward pass
 new_w1 = w1.assign(w1 - learning_rate * grad_w1)
 new_w2 = w2.assign(w2 - learning_rate * grad_w2)
 
-# Now we have built our computational graph, 
-# so we enter a TensorFlow session to
-# actually execute the graph.
+# execute the graph
 with tf.Session() as sess:
-    # Run the graph once to initialize the Variables w1 and w2.
+    # run the graph once to initialize
     sess.run(tf.global_variables_initializer())
 
-    # Create numpy arrays holding the actual data for the inputs x and targets
-    # y
+    # create numpy arrays holding 
+    # the actual data for x and y
     x_value = np.random.randn(N, D_in)
     y_value = np.random.randn(N, D_out)
     for t in range(500):
-        # Execute the graph many times
-        loss_value, _, _ = sess.run([loss, new_w1, new_w2],
-                                    feed_dict={x: x_value, y: y_value})
+        # execute the graph many times
+        loss_value, _, _ = sess.run(
+            [loss, new_w1, new_w2],
+            feed_dict={x: x_value,
+                       y: y_value})
         if t % 100 == 99:
             print(t, loss_value)
