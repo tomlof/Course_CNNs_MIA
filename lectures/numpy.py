6a9fb1487c9d310466879674ccc904ff8de0c9c8
Minh Vu <minhmanutd@gmail.com>
2020-02-05 15:29:57 +0100

    []

--- lectures/numpy.py
+++ lectures/numpy.py
@@ -14,7 +14,7 @@ w2 = np.random.randn(H, D_out)
 
 learning_rate = 1e-6
 for t in range(500):
-    # Forward pass: compute predicted y
+    # Forward pass
     h = x.dot(w1)
     h_relu = np.maximum(h, 0)
     y_pred = h_relu.dot(w2)
@@ -23,7 +23,7 @@ for t in range(500):
     loss = np.square(y_pred - y).sum()
     print(t, loss)
 
-    # Backprop to compute gradients of w1 and w2 with respect to loss
+    # Backprop to compute gradients
     grad_y_pred = 2.0 * (y_pred - y)
     grad_w2 = h_relu.T.dot(grad_y_pred)
     grad_h_relu = grad_y_pred.dot(w2.T)
